235e3749ca1ed3190f6e3589c25d3dfaa09e3160
S.K Shuvo <zamiulbasedshuvo444@gmail.com>
2023-05-05 18:47:00 +0600

    login form added

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './Footer.css'
-import { FaBeer, FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
     return (
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,50 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useState } from 'react';
+import './Login.css'
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleEmailChange = (e) => {
+
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Do something with username and password, e.g. send them to a server to verify
+        console.log("Username:", username);
+        console.log("Password:", password);
+    };
+
     return (
-        <div>
-            <h2>this is login</h2>
+        <div className='login-container' >
+            <form onSubmit={handleSubmit}>
+                <input className='mb-3' type="text" name="" id="" placeholder='Your name' /><br />
+                <input className='mb-3' onChange={handleEmailChange} type="email" name="email" placeholder='Enter your email' />
+                <br />
+                <input  className='mb-3'
+                    onChange={handlePasswordChange}
+                    type="password"
+                    value={password}
+                    placeholder='Enter your password'
+
+                />
+
+                <br />
+                <button className='mb-3 ms-5 rounded text-white bg-primary'  type="submit">Submit</button>
+            </form>
         </div>
     );
 };
+
+
+
+
 
 export default Login;
